55f7f7c3ad6877c06ca5ef8cd2a062bccf5a2972
Riley Smith <gordonrileysmith@gmail.com>
2021-09-29 15:02:51 -0600

    added loading animals in animals page

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -62,7 +64,7 @@ function createNewAnimal(body, animalsArray) {
     animalsArray.push(animal);
     fs.writeFileSync(
         path.join(__dirname, './data/animals.json'),
-        json.stringify({animals: animalsArray}, null, 2)
+        JSON.stringify({animals: animalsArray}, null, 2)
     );
     return animal;
 }
@@ -113,6 +115,22 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     } 
   });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public.index.html'))
+})
 
 app.listen(PORT, () => {
     console.log('API server now on port 3001');
